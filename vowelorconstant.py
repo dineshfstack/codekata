2768a6c65b07392e8039c01c50a28b959d2eb0cb
Dinesh <test@mail.com>
2019-08-10 15:59:39 +0530

    vowel or contsant porgram code kata set 1 problem 3

---
+++ vowelorconstant.py
@@ -0,0 +1,5 @@
+ch=input("Enter your character:")
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+    print("Alphabet")
+else:
+    print("No")
