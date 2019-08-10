efca0549026a50e5bc67b12557cf538cf86d8c65
Dinesh <test@mail.com>
2019-08-10 16:44:34 +0530

    vowel or contsant porgram code kata set 1 problem 3

--- vowelorconstant.py
+++ vowelorconstant.py
@@ -1,4 +1,4 @@
-ch11=input()
+ch1=input()
 if(ord(ch1)==97 or ord(ch1)==101 or ord(ch1)==105 or ord(ch1)==111 or ord(ch1)==117 or ord(ch1)==65 or ord(ch1)==69 or ord(ch1)==73 or ord(ch1)==79 or ord(ch1)==85):
     print("Vowel")
 elif((ord(ch1)>=65 and ord(ch1)<=90) or(ord(ch1)>=97 and ord(ch1)<=122)):
